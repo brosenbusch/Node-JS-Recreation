feb5801ab115a91f592c535100cf7a8e560d4567
brosenbusch <8brosenbusch@stpatrick.org>
2017-04-20 14:00:58 -0500

    Add files via upload

--- hover.js
+++ hover.js
@@ -1,8 +1,26 @@
 (function(){
     const doc = document;
-    let pic1 = doc.querySelector("#first div div img")
+    let pic1 = doc.querySelector("#first div")
+    let leftdiv = doc.querySelector("#second div div #left div")
+    let rightdiv = doc.querySelector("#second div div #right div")
 
-    pic1.addEventListener("hover",function(
-        
-    ))
-})()
+
+    pic1.addEventListener("mouseover",function(){
+        pic1.style.border = "20px solid green";
+    });
+    pic1.addEventListener("mouseout",function(){
+        pic1.style.border = "20px solid transparent";
+    });
+    leftdiv.addEventListener("mouseover",function(){
+        leftdiv.style.opacity = 0.8;
+    });
+    leftdiv.addEventListener("mouseout",function(){
+        leftdiv.style.opacity = 1;
+    });
+    rightdiv.addEventListener("mouseover",function(){
+        rightdiv.style.opacity = 0.8;
+    });
+    rightdiv.addEventListener("mouseout",function(){
+        rightdiv.style.opacity = 1;
+    });
+})();
